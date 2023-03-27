668dcd4e61da43f4668d80547b121b0c7b50fb3a
YunjiCho <76531885+YunjiCho@users.noreply.github.com>
2023-03-27 09:14:47 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -53,7 +53,7 @@ function preload() {
 function setup() {
   // frameRate(30);
   canvas = createCanvas(960, 960);
-  canvas.position(310, 10);
+  canvas.position(310+80, 10);
 
   for (let i = 0; i < 400; i++) {
     circles[i] = new Circle(
